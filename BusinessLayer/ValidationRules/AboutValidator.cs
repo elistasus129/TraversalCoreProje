5076744ec2b4197ed2b856be538847831570a4e3
elistasus129 <61631884+elistasus129@users.noreply.github.com>
2023-01-08 18:57:30 +0300

    Add project files.

---
+++ BusinessLayer/ValidationRules/AboutValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class AboutValidator:AbstractValidator<About>
+    {
+        public AboutValidator()
+        {
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Açıklama kısmını boş geçemezsiniz...!");
+            RuleFor(x => x.Image1).NotEmpty().WithMessage("Açıklama görsel seçinz...!");
+            RuleFor(x => x.Description).MinimumLength(50).WithMessage("Lüften en az 50 karakterlik açıklama bilgisi giriniz");
+            RuleFor(x => x.Description).MaximumLength(150).WithMessage("Lüften açıklamayaı kısaltın");
+
+        }
+    }
+}
